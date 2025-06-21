c2d73deb302ccd071db5a5b0d2e362a7e7c8cdd4
pd0rk1n <patrick.dorkin@hotmail.com>
2025-06-20 23:20:15 -0400

    update

--- setup-git-v5.sh
+++ setup-git-v5.sh
@@ -2,15 +2,15 @@
 
 project=$(basename `pwd`)
 echo "-----------------------------------------------------------------------------"
-echo "this is project https://github.com/kirodubes/"$project
+echo "this is project https://github.com/Pd0rk1n/"$project
 echo "-----------------------------------------------------------------------------"
 git config --global pull.rebase false
-git config --global user.name "Kiro Dubes"
-git config --global user.email "kirodubes@gmail.com"
+git config --global user.name "Pd0rk1n"
+git config --global user.email "patrick.dorkin@hotmail.com"
 sudo git config --system core.editor nano
 git config --global push.default simple
 
-git remote set-url origin git@github.com:kirodubes/$project
+git remote set-url origin git@github.com:Pd0rk1n/$project
 
 echo "Everything set"
 
