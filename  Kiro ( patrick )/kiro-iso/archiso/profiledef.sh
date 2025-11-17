bb609085a24129932d70dfad18090f5831c77733
Pd0rk1n <patrick.dorkin@hotmail.com>
2025-11-17 18:59:30 -0500

    updated again

---  Kiro ( patrick )/kiro-iso/archiso/profiledef.sh
+++  Kiro ( patrick )/kiro-iso/archiso/profiledef.sh
@@ -10,9 +10,9 @@ install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux'
            'uefi.systemd-boot')
-arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86')
 #airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '6')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
